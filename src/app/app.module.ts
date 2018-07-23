f8fa83e43df223f93461c2a540c8bed681680e71
witflash <witflash@gmail.com>
2018-07-24 00:23:21 +0300

    init main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from "@angular/router";
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'favorites', component: FavoriteComponent},
+  {path: '**', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FavoriteComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
